41678867b20041b56a727fe3dafd9542743645a3
Darrian Bagley <dbagle14@gmail.com>
2022-04-27 15:55:21 -0400

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum odio laboriosam aliquid ad delectus saepe suscipit nostrum est sunt. Eligendi dignissimos quidem, explicabo hic nulla incidunt enim id amet maiores.</p>
+    <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
